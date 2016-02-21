ae029b1b08efd0921897e60dae1d67fce7d61c57
omaflak <omaflak@yahoo.fr>
2016-02-21 17:38:31 +0100

    add error callback in client

--- main_client.cpp
+++ main_client.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <winsock2.h>
-#include <windows.h>
 #include "SocketClient.h"
 
 using namespace std;
 
+void onError(errorStruct *e)
+{
+    cout << e->message << endl;
+}
+
 int main()
 {
     SocketClient client("127.0.0.1", 5555);
+    client.setErrorCallback(onError);
     client.connect();
 
     string str;
